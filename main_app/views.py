2a03fdbd87faef935c8a979ad3ab698cd976ae4d
leov1963 <leov1963@gmail.com>
2021-03-19 15:33:40 -0700

    Add index, about, and contact pages

--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Cat Collector</h1>')
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+    
+
+# 1. Make a view function
+# 2. Make the html page
+# 3. Add the view to the urls.py inside of main_app.urls
+# In browser
+# When I go to localhost:8000/contact
+# Django -> urls -> /contact -> vews.contact (runs function) -> templates -> contact.html
